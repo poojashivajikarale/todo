86dd3bdb27f7095559b4975cb77d87774ca1ff1f
Pooja Karale <pooja@hulkapps.com>
2021-07-20 11:54:02 +0530

    adding secret key

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+config.secret_key = 'bdebec730d2cbcb266081b516fb852556d9266a11ba336f455d2ce57d8ccabdcb781601ff966c27ee0599202f6bb21b7e68237b28dcd65f779c68eaa6a812d2b'
+
+
 module Todo
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
