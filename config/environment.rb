3d76056452cf794bceab3fd17d9965a7a65bf22b
Pooja Karale <pooja@hulkapps.com>
2021-07-20 11:56:30 +0530

    move secret key to env rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+config.secret_key = 'bdebec730d2cbcb266081b516fb852556d9266a11ba336f455d2ce57d8ccabdcb781601ff966c27ee0599202f6bb21b7e68237b28dcd65f779c68eaa6a812d2b'
+
 # Initialize the Rails application.
 Rails.application.initialize!
